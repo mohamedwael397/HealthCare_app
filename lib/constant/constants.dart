a75c188866f176b6300b756421df0f9e2f725855
mohamedwael397 <amedwael849@gmail.com>
2025-11-09 01:22:20 +0200

    feature(Login-screen): feature Login screen without Register

---
+++ lib/constant/constants.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+typedef MyValidator = String? Function(String?);
+
+class TextFormFieldWidget extends StatefulWidget {
+  final TextInputType keyboardType;
+  final String? hintText;
+  bool obscureText;
+  final bool isPassword;
+  final TextEditingController controller;
+  final MyValidator validator;
+  final Widget? prefixIcon; // Changed to Widget to support SvgPicture or Icon
+
+  TextFormFieldWidget({
+    required this.controller,
+    required this.validator,
+    this.hintText,
+    this.prefixIcon,
+    this.keyboardType = TextInputType.text,
+    this.obscureText = false,
+    this.isPassword = false,
+    super.key,
+  });
+
+  @override
+  State<TextFormFieldWidget> createState() => _TextFormFieldWidgetState();
+}
+
+class _TextFormFieldWidgetState extends State<TextFormFieldWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      style: const TextStyle(
+        fontSize: 18,
+        fontWeight: FontWeight.w400,
+        color: Colors.black,
+        overflow: TextOverflow.ellipsis,
+      ),
+      obscureText: widget.obscureText,
+      keyboardType: widget.keyboardType,
+      controller: widget.controller,
+      validator: widget.validator,
+      decoration: InputDecoration(
+        hintText: widget.hintText,
+        hintStyle: const TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.w400,
+          color: Colors.grey,
+          overflow: TextOverflow.ellipsis,
+        ),
+        prefixIcon: widget.prefixIcon,
+        suffixIcon: widget.isPassword
+            ? IconButton(
+                icon: Icon(
+                  widget.obscureText ? Icons.visibility_off : Icons.visibility,
+                  color: const Color(0xff454A4F),
+                  size: 24,
+                ),
+                onPressed: () {
+                  setState(() {
+                    widget.obscureText = !widget.obscureText;
+                  });
+                },
+              )
+            : null,
+        contentPadding: const EdgeInsets.all(15),
+        enabledBorder: outlineInputBorder(
+          color: const Color(0xffBABABA),
+          radius: 10,
+          width: 1,
+        ),
+        focusedBorder: outlineInputBorder(
+          color: const Color(0xFF407CE2),
+          radius: 10,
+          width: 1,
+        ),
+        errorBorder: outlineInputBorder(
+          color: Colors.red,
+          radius: 10,
+          width: 1,
+        ),
+        focusedErrorBorder: outlineInputBorder(
+          color: Colors.red,
+          radius: 10,
+          width: 1,
+        ),
+      ),
+    );
+  }
+
+  OutlineInputBorder outlineInputBorder({
+    required double radius,
+    required Color color,
+    required double width,
+  }) {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(radius),
+      borderSide: BorderSide(color: color, width: width),
+    );
+  }
+}
